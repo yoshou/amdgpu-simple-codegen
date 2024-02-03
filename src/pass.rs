99c1bae123dc343dcf7891da21b7a2dbbcb2e3bc
yoshou <yoshou.dev@gmail.com>
2024-02-03 21:10:29 +0900

    feat: add region pass

--- src/pass.rs
+++ src/pass.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-use crate::ir::{Function, Module};
+use crate::{ir::{Function, Module}, region::{Region, RegionInfo}};
 
 pub trait ModulePass {
     fn run_on_module(&mut self);
@@ -9,6 +9,15 @@ pub trait FunctionPass {
     fn run_on_function(
         &mut self,
         function: Rc<RefCell<Function>>,
+        module: &mut Module,
+    ) -> Option<Rc<RefCell<Function>>>;
+}
+pub trait RegionPass {
+    fn run_on_region(
+        &mut self,
+        region: Rc<RefCell<Region>>,
+        function: Rc<RefCell<Function>>,
+        region_info: &RegionInfo,
         module: &mut Module,
     ) -> Option<Rc<RefCell<Function>>>;
 }
